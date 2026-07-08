4b41096a23f816939ce6ee2f12d571d84be2ab6a
AddecheRyan <rnaddeche@wpi.edu>
2026-07-08 12:52:21 -0400

    User is redirected to login from / if not logged in

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,23 @@
+"use client"
 
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/dashboard");
+  }, []);
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
-
+      <span className="text-[200px] font-bold">JUMPSCARE</span>
+      <img
+                src="/clown.svg"
+                alt="Jumpscare!"
+                className="h-[600px] w-[1200px]"
+            />
     </div>
   );
 }
